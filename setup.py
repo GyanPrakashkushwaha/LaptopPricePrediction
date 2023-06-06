a2543feea632e829a797fe50cd194070cf1fc218
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-06-06 10:53:04 +0530

    exception and logger

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages , setup
+
+
+setup(
+    name='Laptop_Price-Prediction',
+    version='0.0.1',
+    author='GyanPrakashKushwaha',
+    author_email='gyanp7880@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
+
+
+
