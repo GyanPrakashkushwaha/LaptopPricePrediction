d6ad865d19445182427fca72c17a16cbcecc95fd
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-06-09 11:23:26 +0530

    testing

--- src/Components/Testing.py
+++ src/Components/Testing.py
@@ -9,13 +9,13 @@ warnings.filterwarnings('ignore')
 obj = DataCollection()
 obj.initiate_data_collection()
 
-# train_df ,test_df=obj.initiate_data_collection()
+train_df ,test_df=obj.initiate_data_collection()
 
-# data_trans = DataTransformation(train_data_path=train_df,test_data_path=test_df)
+data_trans = DataTransformation(train_data_path=train_df,test_data_path=test_df)
 
 
-# train_arr , test_arr ,_=data_trans.initiate_data_transformation()
+train_arr , test_arr ,_=data_trans.initiate_data_transformation()
 
-# train_model = ModelTrainer(train_arr=train_arr,test_arr=test_arr)
+train_model = ModelTrainer(train_arr=train_arr,test_arr=test_arr)
 
-# train_model.initiate_model_training()
+train_model.initiate_model_training()
